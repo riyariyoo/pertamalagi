ab99e10f857e748bcfbd23154e8b55b4699212d7
umriya afini <riyariyoo@riyariyoo-Aspire-4741.(none)>
2014-04-10 19:04:35 +0700

    source code matematika.java

---
+++ Matematika.java
@@ -0,0 +1,19 @@
+public class Matematika{
+	int a,b;
+	
+	void pertambahan(int a, int b){
+		System.out.println(a+" + "+b+" = "+(a+b));
+	}
+
+	void perkalian(int a, int b){
+		System.out.println(a+" x "+b+" = "+(a*b));
+	}
+
+	void pembagian(int a, int b){
+		System.out.println(a+" : "+b+" = "+(a/b));
+	}
+
+	void pengurangan (int a, int b){
+		System.out.println(a+" - "+b+" = "+(a-b));
+	}
+}
